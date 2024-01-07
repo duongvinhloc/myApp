b094cc399e8f485f4925ce22be32ca762e61f69c
duongvinhloc <duongvinhloc@gmail.com>
2024-01-08 00:58:48 +0900

    Done authencation with sanctum , bearer token

--- api-app/database/migrations/2024_01_07_153832_add_user_id_to_tasks.php
+++ api-app/database/migrations/2024_01_07_153832_add_user_id_to_tasks.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->boolean('is_completed')->default(false);
-            //$table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete()->after('id');
+
         });
     }
 
@@ -25,6 +22,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeignId('user_id');
+            $table->dropColumn('user_id');
+
+        });
     }
 };
--- api-app/routes/api.php
+++ api-app/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 /*
@@ -15,10 +18,18 @@ use App\Http\Controllers\Api\V1\CompleteTaskController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::prefix('v1')->group(function(){
-    Route::apiResource('/tasks', TaskController::class);
-    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+
+require __DIR__ .'/api/v1.php';
+require __DIR__ .'/api/v2.php';
+
+
+Route::prefix('auth')->group(function(){
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+
 });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
